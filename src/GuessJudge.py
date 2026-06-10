509998f0c2ff82f429897552f16b59f4670adf68
TAKAHIRO <takahiro5138@gmail.com>
2026-06-10 11:22:36 +0900

    フォーマット整形

--- src/GuessJudge.py
+++ src/GuessJudge.py
@@ -1,15 +1,15 @@
 from GuessResult import GuessResult
 
-class GuessJudge:
 
-    def judge(self,guess, answer):
+class GuessJudge:
+    def judge(self, guess, answer):
         if guess == answer:
             return "Correct"
         elif len(guess) < len(answer):
             return "Too few characters"
         elif len(guess) > len(answer):
             return "Too many characters"
-        
+
         answer = list(answer)
         guess = list(guess)
 
@@ -18,16 +18,16 @@ class GuessJudge:
         for i in range(len(answer)):
             if guess[i] == answer[i]:
                 hit += 1
-                #この場合guessからgを削除する、削除して配列がずれないようにnullを入れる
+                # この場合guessからgを削除する、削除して配列がずれないようにnullを入れる
                 guess[i] = None
                 answer[i] = None
-        
+
         for j in answer:
             if j is None:
                 continue
             if j in guess:
                 blow += 1
-                #この場合guessからgを削除する、削除して配列がずれないようにnullを入れる
+                # この場合guessからgを削除する、削除して配列がずれないようにnullを入れる
                 guess[guess.index(j)] = None
 
         return GuessResult(hit, blow)
