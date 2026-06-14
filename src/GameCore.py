170ee21f265609648beae8efd70d47a5a130d670
Yuu8600 <yusu0016@gmail.com>
2026-06-14 23:08:53 +0900

    GameCore の MVP 実装

--- src/GameCore.py
+++ src/GameCore.py
@@ -7,24 +7,24 @@ class GameCore:
         self._wordProvider = WordProvider()
         self._judge = GuessJudge()
         self._view = CliView()
-        
-        self._answer = self._wordProvider.getAnswer()
-        
+
+        self._answer = self._wordProvider.GetAnswer()
+
     def play(self):
+        self._view.ShowStart()
+
         while True:
-            #プレーヤーの入力を受け取る
-            guess = self._view.readGuess()
-            
-            result = self._judge(self._answer, guess)
-            
-            
-            
-            # CliView を使って判定結果（ヒット数・ブロー数）を表示
-            self._view.showGuessResult(result)
-            
-            # もし4ヒットならループを抜けてクリア処理へ
-            if result.getHits() == 4:
-                self._view.showSuccess()
+            guess = self._view.ReadGuess()
+
+            result = self._judge.judge(guess, self._answer)
+
+            self._view.ShowGuessResult(result)
+
+            if result.GetHits() == 4:
+                self._view.ShowSuccess()
                 break
 
-            
+
+if __name__ == "__main__":
+    game = GameCore()
+    game.play()
